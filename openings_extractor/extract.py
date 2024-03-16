500f36a1f58e6a77b0be230a334d115e0c7173d2
funnsam <funnsam8@gmail.com>
2024-03-16 23:41:10 +0800

    a

--- openings_extractor/extract.py
+++ openings_extractor/extract.py
@@ -1,19 +1,28 @@
+from bloom_filter2 import BloomFilter
 import chess
 import chess.engine
 import chess.pgn
+import chess.polyglot
 import io
 import itertools
 
-NUM_MOVES = 4
+NUM_MOVES = 6
 NUM_HALFMOVES = NUM_MOVES * 2
 ROUGHLY_EQ_CENTIPAWNS = 75
 
+EXTRACT_POSITIONS = 4096
+
 db = open("./db.pgn")
-db = db.read()
-total = db.count("[Event")
-db = io.StringIO(db)
+total = 179550
 
 fen = ""
+
+def hasher(bloom_filter, key):
+    for probeno in range(1, bloom_filter.num_probes_k + 1):
+        yield key % bloom_filter.num_bits_m
+        key = int(key / bloom_filter.num_bits_m)
+
+visited = BloomFilter(max_elements = total, error_rate = 0.1, probe_bitnoer = hasher)
 
 engine = chess.engine.SimpleEngine.popen_uci("stockfish")
 
@@ -21,9 +30,11 @@ def analyse(game, time_limit = 0.01):
     result = engine.analyse(board, chess.engine.Limit(time=time_limit))
     return result['score']
 
-print()
+print(total)
 
 i = 0
+j = 0
+
 while True:
     game = chess.pgn.read_game(db)
 
@@ -38,17 +49,27 @@ while True:
         board.push(m)
         moves += 1
 
-    if moves < NUM_HALFMOVES:
+    h = chess.polyglot.zobrist_hash(board)
+
+    i += 1
+
+    if moves < NUM_HALFMOVES or h in visited:
         continue
+
+    visited.add(h)
 
     score = analyse(board)
 
-    i += 1
-    if abs(score.relative.score(mate_score = 10000)) < ROUGHLY_EQ_CENTIPAWNS and board.fen() not in fen:
+    if abs(score.relative.score(mate_score = 10000)) < ROUGHLY_EQ_CENTIPAWNS:
         fen += board.fen()
         fen += "\n"
 
-        print("\x1b[1A{}/{}".format(i, total))
+        j += 1
+
+        print("\x1b[1A{}/{} ({:.2f}%) extracted {}".format(i, total, 100 * i / total, j))
+
+        if j >= EXTRACT_POSITIONS:
+            break
 
 db.close()
 engine.close()
