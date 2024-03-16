02a401e82270e32d7dcf4637bdb1be40de66fc25
funnsam <funnsam8@gmail.com>
2024-03-16 06:23:51 +0000

    a

---
+++ openings_extractor/extract.py
@@ -0,0 +1,47 @@
+import chess
+import chess.engine
+import chess.pgn
+import itertools
+
+db = open("./db.pgn")
+fen = ""
+
+engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+
+def analyse(game, time_limit = 0.01):
+    result = engine.analyse(board, chess.engine.Limit(time=time_limit))
+    return result['score']
+
+print()
+
+i = 0
+while True:
+    game = chess.pgn.read_game(db)
+
+    if not game:
+        break
+
+    board = game.board()
+
+    for m in itertools.islice(game.mainline_moves(), 8):
+        board.push(m)
+
+    score = analyse(board)
+
+    if abs(score.relative.score(mate_score = 10000)) < 100 and board.fen() not in fen:
+        fen += board.fen()
+        fen += "\n"
+
+        i += 1
+
+        print("\x1b[1A{}".format(i))
+
+        if i > 256:
+            break
+
+db.close()
+engine.close()
+
+openings = open("../openings.txt", "w")
+openings.write(fen)
+openings.close()
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::sync::atomic::*;
 use std::str::FromStr;
 use std::process::*;
 use std::io::{self, BufRead as _, Write as _};
+use std::time::*;
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -14,12 +15,21 @@ struct Args {
 
     #[arg(long, default_value = "openings.txt")]
     opening_positions: String,
+
+    #[arg(long, action = ArgAction::SetTrue)]
+    biased: bool,
+
+    #[arg(short = 'n', long, default_value_t = usize::MAX)]
+    play_positions: usize,
+
+    #[arg(short, long, default_value_t = 1)]
+    jobs: usize,
 }
 
-const BAR_LENGTH: usize = 20;
 static THREADS: AtomicUsize = AtomicUsize::new(0);
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
     let game_result = [AtomicUsize::new(0), AtomicUsize::new(0), AtomicUsize::new(0)]; // a win | draw | b win
 
@@ -27,6 +37,7 @@ fn main() {
         .unwrap()
         .trim()
         .lines()
+        .take(args.play_positions)
         .map(|fen| (chess::Game::from_str(fen).unwrap(), fen.to_string()))
         .collect::<Vec<(chess::Game, String)>>();
 
@@ -36,30 +47,45 @@ fn main() {
         let fen = unsafe { core::mem::transmute::<_, &'static _>(fen.as_str()) };
         let game_result = unsafe { core::mem::transmute::<_, &'static [AtomicUsize; 3]>(&game_result) };
 
-        play(a, b, game.clone(), fen, args.time, args.inc, game_result, false);
-        play(b, a, game.clone(), fen, args.time, args.inc, game_result, true);
+        play(a, b, game.clone(), fen, args.time, args.inc, game_result, false, args.jobs).await;
+
+        if !args.biased {
+            play(b, a, game.clone(), fen, args.time, args.inc, game_result, true, args.jobs).await;
+        }
     }
 
     while THREADS.load(Ordering::Relaxed) != 0 {
         core::hint::spin_loop();
     }
 
-    let total = fens.len() * 2;
+    let total = fens.len() * (2 - args.biased as usize);
 
     let a = game_result[0].load(Ordering::Relaxed);
     let d = game_result[1].load(Ordering::Relaxed);
     let b = game_result[2].load(Ordering::Relaxed);
 
-    let a_bar_length = ((a as f32 / total as f32) * BAR_LENGTH as f32) as usize;
-    let d_bar_length = ((d as f32 / total as f32) * BAR_LENGTH as f32) as usize;
-    let b_bar_length = ((b as f32 / total as f32) * BAR_LENGTH as f32) as usize;
+    let term_size = term_size::dimensions().unwrap_or((80, 24));
+    let bar_length = term_size.0 - 4;
 
-    let a_bar = "=".repeat(a_bar_length);
-    let d_bar = "=".repeat(d_bar_length);
-    let b_bar = "=".repeat(b_bar_length);
+    let a_bar_length = ((a as f32 / total as f32) * bar_length as f32) as usize;
+    let d_bar_length = ((d as f32 / total as f32) * bar_length as f32) as usize;
+    let b_bar_length = ((b as f32 / total as f32) * bar_length as f32) as usize;
 
+    let a_bar = "━".repeat(a_bar_length);
+    let d_bar = "━".repeat(d_bar_length);
+    let b_bar = "━".repeat(b_bar_length);
+
+    let indents_length = term_size.0 - " SUMMARY ".len();
+    let l_indent_length = indents_length / 2;
+    let r_indent_length = indents_length - l_indent_length;
+
+    let l_indent = "=".repeat(l_indent_length);
+    let r_indent = "=".repeat(r_indent_length);
+
+    println!("\n\n\x1b[1m{} SUMMARY {}\x1b[0m", l_indent, r_indent);
     println!("\x1b[1mTotal:\x1b[0m {total} games");
-    println!("\x1b[32mA ({}) {}\x1b[90m{}\x1b[31m{} B ({})\x1b[0m", a, a_bar, d_bar, b_bar, b);
+    println!("   \x1b[32mA wins: {a}\x1b[90m | Draws: {d} | \x1b[31mA loses: {b}\x1b[0m");
+    println!("  \x1b[32m{a_bar}\x1b[90m{d_bar}\x1b[31m{b_bar}\x1b[0m");
 }
 
 fn flip(idx: usize, flip: bool) -> usize {
@@ -70,9 +96,13 @@ fn flip(idx: usize, flip: bool) -> usize {
     }
 }
 
-fn play(a: &'static str, b: &'static str, mut game: chess::Game, fen: &'static str, time: usize, inc: usize, game_result: &'static [AtomicUsize; 3], polarity: bool) {
+async fn play(a: &'static str, b: &'static str, mut game: chess::Game, fen: &'static str, time: usize, inc: usize, game_result: &'static [AtomicUsize; 3], polarity: bool, jobs: usize) {
+    while THREADS.load(Ordering::Relaxed) >= jobs {
+        core::hint::spin_loop();
+    }
+
     THREADS.fetch_add(1, Ordering::Relaxed);
-    std::thread::spawn(move || {
+    tokio::spawn(async move {
         let mut a_engine = Engine::new(a, fen);
         let mut b_engine = Engine::new(b, fen);
 
@@ -81,31 +111,45 @@ fn play(a: &'static str, b: &'static str, mut game: chess::Game, fen: &'static s
         let mut overtime = 0;
 
         'a: while game.result().is_none() {
-            if !a_engine.get_move(&mut game, &mut tc, inc) {
+            if !a_engine.get_move(&mut game, &mut tc, inc).await {
                 overtime = 1;
                 break 'a;
             }
 
-            if !b_engine.get_move(&mut game, &mut tc, inc) {
+            if game.can_declare_draw() {
+                game.declare_draw();
+            }
+
+            if game.result().is_some() {
+                break 'a;
+            }
+
+            if !b_engine.get_move(&mut game, &mut tc, inc).await {
                 overtime = 2;
                 break 'a;
+            }
+
+            if game.can_declare_draw() {
+                game.declare_draw();
             }
         }
 
         if overtime == 1 {
-            game_result[flip(0, polarity)].fetch_add(1, Ordering::Relaxed);
-        } else if overtime == 2 {
             game_result[flip(2, polarity)].fetch_add(1, Ordering::Relaxed);
+        } else if overtime == 2 {
+            game_result[flip(0, polarity)].fetch_add(1, Ordering::Relaxed);
         } else {
             match game.result() {
                 Some(chess::GameResult::WhiteCheckmates) => { game_result[flip(0, polarity)].fetch_add(1, Ordering::Relaxed); },
                 Some(chess::GameResult::BlackCheckmates) => { game_result[flip(2, polarity)].fetch_add(1, Ordering::Relaxed); },
-                Some(chess::GameResult::Stalemate) => { game_result[1].fetch_add(1, Ordering::Relaxed); },
+                Some(chess::GameResult::DrawAccepted | chess::GameResult::DrawDeclared | chess::GameResult::Stalemate) => { game_result[1].fetch_add(1, Ordering::Relaxed); },
                 _ => unreachable!(),
             }
         }
 
-        println!("ok");
+        export_pgn(&game, if !polarity { a } else { b }, if polarity { a } else { b }, fen);
+
+        println!("\x1b[1;32mInfo:\x1b[0m a game was ended");
 
         THREADS.fetch_sub(1, Ordering::Relaxed);
     });
@@ -116,12 +160,18 @@ struct Engine<'a> {
     fen: &'a str,
 }
 
+impl<'a> Drop for Engine<'a> {
+    fn drop(&mut self) {
+        _ = self.exec.kill();
+    }
+}
+
 impl<'a> Engine<'a> {
     fn new(exec: &str, fen: &'a str) -> Self {
         let mut exec = std::process::Command::new(exec)
             .stdin(std::process::Stdio::piped())
             .stdout(std::process::Stdio::piped())
-            .stdout(std::process::Stdio::null())
+            .stderr(std::process::Stdio::null())
             .spawn()
             .unwrap();
         writeln!(exec.stdin.as_ref().unwrap(), "uci").unwrap();
@@ -136,9 +186,7 @@ impl<'a> Engine<'a> {
         }
     }
 
-    fn get_move(&mut self, game: &mut chess::Game, tc: &mut (usize, usize), inc: usize) -> bool {
-        use std::time::*;
-
+    async fn get_move(&mut self, game: &mut chess::Game, tc: &mut (usize, usize), inc: usize) -> bool {
         let tc0 = tc.0;
         let tc1 = tc.1;
 
@@ -166,28 +214,38 @@ impl<'a> Engine<'a> {
             tc1
         ).unwrap();
 
-        let mut m = chess::ChessMove::default();
-        for l in io::BufReader::new(self.exec.stdout.as_mut().unwrap()).lines() {
-            if let Ok(l) = l {
-                let mut tokens = l.split_whitespace();
-                if matches!(tokens.next(), Some("bestmove")) {
-                    m = move_from_uci(&tokens.next().unwrap());
-                    break;
+        let m = tokio::time::timeout(
+            Duration::from_millis(*mt as u64),
+            async move {
+                let mut lines = io::BufReader::new(self.exec.stdout.as_mut().unwrap()).lines();
+                while let Some(Ok(l)) = lines.next() {
+                    let mut tokens = l.split_whitespace();
+                    if matches!(tokens.next(), Some("bestmove")) {
+                        return Some(move_from_uci(&tokens.next().unwrap()));
+                    }
                 }
+
+                None
             }
-        }
+        ).await;
 
-        let used_time = start.elapsed().as_millis() as usize;
+        m.ok().flatten().map_or(false, |m| {
+            let used_time = start.elapsed().as_millis() as usize;
 
-        game.make_move(m);
+            if !game.current_position().legal(m) {
+                return false;
+            }
 
-        let time = (*mt + inc).checked_sub(used_time);
-        if let Some(time) = time {
-            *mt = time;
-            true
-        } else {
-            false
-        }
+            game.make_move(m);
+
+            let time = (*mt + inc).checked_sub(used_time);
+            if let Some(time) = time {
+                *mt = time;
+                true
+            } else {
+                false
+            }
+        })
     }
 }
 
@@ -219,4 +277,116 @@ fn move_from_uci(m: &str) -> chess::ChessMove {
     });
 
     chess::ChessMove::new(src, dst, piece)
+}
+
+fn export_pgn(game: &chess::Game, w: &str, b: &str, fen: &str) {
+    use std::fmt::Write as _;
+
+    let mut pgn = String::new();
+    writeln!(pgn, r#"[Event "AB test"]"#).unwrap();
+    writeln!(pgn, r#"[Site "https://github.com/funnsam/uci-ab-test"]"#).unwrap();
+    writeln!(pgn, r#"[Date "??"]"#).unwrap();
+    writeln!(pgn, r#"[Round "??"]"#).unwrap();
+    writeln!(pgn, r#"[White "{w}"]"#).unwrap();
+    writeln!(pgn, r#"[Black "{b}"]"#).unwrap();
+
+    let result = match game.result() {
+        Some(chess::GameResult::BlackResigns | chess::GameResult::WhiteCheckmates) => "1-0",
+        Some(chess::GameResult::WhiteResigns | chess::GameResult::BlackCheckmates) => "0-1",
+        Some(_) => "1/2-1/2",
+        None => "*",
+    };
+
+    writeln!(pgn, r#"[Result "{result}"]"#).unwrap();
+    writeln!(pgn, r#"[FEN "{fen}"]"#).unwrap();
+    writeln!(pgn).unwrap();
+
+    let mut board = chess::Board::from_str(fen).unwrap();
+
+    for (i, m) in game.actions().iter().filter_map(|a| match a { chess::Action::MakeMove(m) => Some(m), _ => None }).collect::<Vec<&chess::ChessMove>>().chunks(2).enumerate() {
+        write!(pgn, "{}. ", i + 1).unwrap();
+
+        for m in m {
+            let m = make_san(&mut board, **m);
+
+            if pgn.lines().last().unwrap().len() + m.len() >= 100 {
+                pgn = pgn.trim_end().to_string();
+                writeln!(pgn).unwrap();
+            }
+
+            write!(pgn, "{m} ").unwrap();
+        }
+    }
+
+    pgn += result;
+
+    std::fs::write(format!("game_{}.pgn", UNIX_EPOCH.elapsed().unwrap().as_millis()), pgn).unwrap();
+}
+
+fn make_san(board: &mut chess::Board, m: chess::ChessMove) -> String {
+    let cr = board.my_castle_rights();
+    if m.get_source() == board.king_square(board.side_to_move()) && !matches!(m.get_dest().get_file(), chess::File::D | chess::File::G) && matches!(m.get_dest().get_rank(), chess::Rank::First | chess::Rank::Eighth) {
+        if m.get_dest().get_file() < m.get_source().get_file() && cr.has_queenside() {
+            *board = board.make_move_new(m);
+
+            return "O-O-O".to_string();
+        } else if cr.has_kingside() {
+            *board = board.make_move_new(m);
+
+            return "O-O".to_string();
+        }
+    }
+
+    let mut san = String::new();
+
+    let piece = board.piece_on(m.get_source()).unwrap();
+    if piece != chess::Piece::Pawn {
+        san += &piece.to_string(chess::Color::White);
+    };
+
+    if piece != chess::Piece::Pawn {
+        let mask = chess::BitBoard::from_square(m.get_dest());
+        let mut pieces = chess::BitBoard::new(0);
+
+        let mut g = chess::MoveGen::new_legal(board);
+        g.set_iterator_mask(mask);
+        for m in g {
+            pieces |= chess::BitBoard::from_square(m.get_source());
+        }
+
+        pieces &= !(chess::BitBoard::from_square(m.get_source()));
+
+        if pieces.0 != 0 {
+            if (pieces & chess::get_file(m.get_source().get_file())).0 == 0 {
+                san.push((b'a' + m.get_source().get_file() as u8) as char);
+            } else if (pieces & chess::get_rank(m.get_source().get_rank())).0 == 0 {
+                san.push((b'1' + m.get_source().get_rank() as u8) as char);
+            } else {
+                san += &m.get_source().to_string();
+            }
+        }
+    } else if board.piece_on(m.get_dest()).is_some() {
+        san.push((b'a' + m.get_source().get_file() as u8) as char)
+    }
+
+    if board.piece_on(m.get_dest()).is_some() {
+        san += "x";
+    }
+
+    san += &m.get_dest().to_string();
+
+    if let Some(p) = m.get_promotion() {
+        san += "=";
+        san += &p.to_string(chess::Color::White);
+    }
+
+    *board = board.make_move_new(m);
+
+    if matches!(board.status(), chess::BoardStatus::Checkmate) {
+        san += "#";
+    } else if board.checkers().0 != 0 {
+        san += "+";
+    }
+
+    san
 }
